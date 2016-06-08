ba337eea5bf868f0496bf86ba579c76ec784e2f7
Ody Mbegbu <odytrice@gmail.com>
2016-06-08 02:55:05 +0100

    Added Folder Support

---
+++ Pluralsight/Data/strings.fsx
@@ -0,0 +1,9 @@
+﻿"It was the best of times,
+It was the worst of times"
+
+"""This "string" includes double quotes"""
+
+@"This ""string"" includes double quotes"
+
+//Try this with or without the @
+@"Verbatim strings\n don't\t encode\n escape sequences"
